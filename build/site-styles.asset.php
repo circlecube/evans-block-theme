d330320397f00c27b14a3ba5153da8d096a4fabb
Evan Mullins <evanrm@gmail.com>
2022-01-28 16:39:55 -0500

    add about theme snippet to footer

--- build/site-styles.asset.php
+++ build/site-styles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '3f26a192b3f77574b9bafe57132f3af0');
+<?php return array('dependencies' => array(), 'version' => '5318fce56a2eb578026bd876f1671d5a');
